379b6014ad10982b77930852f901adea3d50a3d6
Memoyu <mmy6076@outlook.com>
2022-02-07 23:41:20 +0800

    添加项目文件。

---
+++ LeetCode/1.链表/237. 删除链表中的节点/Program.cs
@@ -0,0 +1,34 @@
+﻿// Topic:https://leetcode-cn.com/problems/delete-node-in-a-linked-list/
+
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int x)
+    {
+        val = x;
+    }
+}
+
+
+public class Solution
+{
+    /// <summary>
+    /// 审题：
+    /// 1、该方法仅传入一个指定要删除的节点，所以只能获取到下个节点；
+    /// 2、需要时链表中有效节点；
+    /// 3、传入的node不能为末尾节点；
+    /// 
+    /// 思路：
+    /// PS：在传统做法中，我们需要获取到当前需要删除节点(node)的 上一个节点(preNode) 及 下一个节点(nextNode)，然后将preNode.next = nextNode进行链接，此时node没有了引用，则达到效果；
+    /// 此题中，无法获取preNode，但可以取到nextNode；此时，我们需要保持preNode对node的next引用，则node不能直接移除，做法则是将nextNode内容(val、next)覆盖到node中，然后移除nextNode；
+    /// </summary>
+    /// <param name="node"></param>
+    public void DeleteNode(ListNode node)
+    {
+        if (node == null || node.next == null) return;
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
