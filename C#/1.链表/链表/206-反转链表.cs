88e40205b94c9eef04dc4cc7435771484ea75e20
Memoyu <mmy6076@outlook.com>
2022-02-10 00:01:37 +0800

    docs：反转链表注释

--- C#/1.链表/链表/206-反转链表.cs
+++ C#/1.链表/链表/206-反转链表.cs
@@ -2,6 +2,15 @@
 
 public class _206_反转链表
 {
+    // Topic: https://leetcode-cn.com/problems/reverse-linked-list/
+
+    /*
+     * 审题：
+     * 1、仅可获得头部节点；
+     * 2、假设有如下链表：1 → 2 → 3 → ∅，反转后得到：∅ ← 1 ← 2 ← 3，由此看出来节点的next指针发生了变化；
+     */
+
+
     /// <summary>
     /// 方案1：迭代
     /// </summary>
