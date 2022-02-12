9b42e17e27948ba8421eb6ab21860840c1569451
Memoyu <mmy6076@outlook.com>
2022-02-12 23:41:31 +0800

    docs：审题、思路

--- C#/1.链表/链表/83-删除排序链表中的重复元素.cs
+++ C#/1.链表/链表/83-删除排序链表中的重复元素.cs
@@ -6,22 +6,29 @@ public class _83_删除排序链表中的重复元素
 
     /*
      * 审题：
-     * 1、
+     * 1、链表为有序
+     * 2、只要碰到当前节点的val与next的val相等，就"删除"当前的next指向即可（改变指向next.next）；
      */
 
 
     public static ListNode DeleteDuplicates(ListNode head)
     {
         if (head == null || head.next == null) return head;
-        ListNode preNode = head;
-        ListNode currNode = head.next;
+        ListNode currNode = head;
         while (currNode != null)
         {
-            if (preNode == currNode)
-                preNode.next = currNode.next;
+            if (currNode.next != null)
+            {
+                if (currNode.val == currNode.next.val)
+                {
+                    currNode.next = currNode.next.next;
+                    continue;
+                }
+                currNode = currNode.next;
+            }
             else
-                preNode = preNode.next;
-            currNode = currNode.next;
+                break;
+                
         }
         return head;
     }
--- C#/1.链表/链表/876-链表的中间结点.cs
+++ C#/1.链表/链表/876-链表的中间结点.cs
@@ -6,9 +6,16 @@ public class _876_链表的中间结点
 
     /*
      * 审题：
-     * 1、
+     * 1、返回链表中中间节点，例如：1 → 2 → 3 → ∅，中间节点为2；1 → 2 → 3 → 4 → ∅，中间节点为3，取左边节点
      */
 
+    /// <summary>
+    /// 思路：
+    /// 引入数组，用于存储遍历的所有节点，最后从数组中取出中间节点；
+    /// 缺点在于如果不确定节点数量时无法使用数组，需要使用动态数组，牺牲空间；
+    /// </summary>
+    /// <param name="head"></param>
+    /// <returns></returns>
     public static ListNode MiddleNode(ListNode head)
     {
         var arr = new ListNode[100];
@@ -21,7 +28,14 @@ public class _876_链表的中间结点
         return arr[i / 2];
     }
 
-
+    /// <summary>
+    /// 思路：
+    /// 单指针法：分为两个步骤进行处理；
+    /// 1、遍历链表，统计链表节点总数n；
+    /// 2、再从头部节点继续遍历，直到 n / 2 时得到的则为中间节点
+    /// </summary>
+    /// <param name="head"></param>
+    /// <returns></returns>
     public static ListNode MiddleNode_1(ListNode head)
     {
         ListNode currNode = head;
@@ -43,6 +57,14 @@ public class _876_链表的中间结点
         return currNode;
     }
 
+    /// <summary>
+    /// 思路：
+    /// 快慢指针法：
+    /// 该方法比较神奇，是对单指针法的进一步优化，此方式只需要遍历一次链表；
+    /// 定义两个节点，初始化为头部节点，在遍历下，slowNode每次向下一个节点，fastNode向下两个节点，当fastNode=null时，则证明节点遍历完成，而slowNode此时正处于中间位置；
+    /// </summary>
+    /// <param name="head"></param>
+    /// <returns></returns>
     public static ListNode MiddleNode_2(ListNode head)
     {
         ListNode slowNode = head;
