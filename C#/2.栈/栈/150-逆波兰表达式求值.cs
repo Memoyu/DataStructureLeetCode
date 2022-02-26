79e5484e3063682aa9530b3515c96879d8e3b0b9
Memoyu <mmy6076@outlook.com>
2022-02-26 22:02:16 +0800

    feat：逆波兰表达式解题

---
+++ C#/2.栈/栈/150-逆波兰表达式求值.cs
@@ -0,0 +1,71 @@
+﻿namespace 栈;
+
+public class _150_逆波兰表达式求值
+{
+    // 顺便了解前缀表达式、中缀表达式、后缀表达式
+    // 中追表达式：通用的算术或逻辑公式表示方法，例如：(1 + 2) * 3
+    // 前缀表达式：又称波兰表达式，是运算表达式，没有括号，运算符写在操作数前边；例如："*, 3, +, 2, 1"；
+    // 后缀表达式：又称逆波兰表达式，是前缀表达式的"反向"表达，运算符写在操作数后边；例如："1, 2, +, 3, *"；
+
+    // Topic: https://leetcode-cn.com/problems/evaluate-reverse-polish-notation/
+
+    /*
+     * 审题：
+     * 1、逆波兰表达式：遵循从左到右，运算符在数值后边；例如："2, 1, +, 3, *" = (2 + 1) * 3；
+     * 2、操作符只有"+, -, /, *"
+     */
+
+    public static int EvalRPN(string[] tokens)
+    {
+        // 使用Equals进行比对实现
+        var stack = new Stack<int>();
+        foreach (var item in tokens)
+        {
+            if (item.Equals("+") || item.Equals("-") || item.Equals("*") || item.Equals("/"))
+            {
+                var p1 = stack.Pop();
+                var p2 = stack.Pop();
+                var com = item.Equals("+") ? p2 + p1 : item.Equals("-") ? p2 - p1 : item.Equals("*") ? p2 * p1 : item.Equals("/") ? p2 / p1 : 0;
+                stack.Push(com);
+            }
+            else
+            {
+                stack.Push(int.Parse(item));
+            }
+        }
+
+        return stack.Pop();
+
+        // 使用Switch 实现，相对会比较耗时
+        //var stack = new Stack<int>();
+        //foreach (var item in tokens)
+        //{
+        //    if (!int.TryParse(item, out int val))
+        //    {
+        //        var p1 = stack.Pop();
+        //        var p2 = stack.Pop();
+        //        switch (item)
+        //        {
+        //            case "+":
+        //                stack.Push(p2 + p1);
+        //                break;
+        //            case "-":
+        //                stack.Push(p2 - p1);
+        //                break;
+        //            case "*":
+        //                stack.Push(p2 * p1);
+        //                break;
+        //            case "/":
+        //                stack.Push(p2 / p1);
+        //                break;
+        //        }
+        //    }
+        //    else
+        //    {
+        //        stack.Push(val);
+        //    }
+        //}
+
+        //return stack.Pop();
+    }
+}
--- C#/DataStructrue.Test/2-栈Test.cs
+++ C#/DataStructrue.Test/2-栈Test.cs
@@ -15,4 +15,17 @@ public class 栈Test
         var res = _20_有效括号.IsValid("([)]");
         Assert.False(res);
     }
+
+    [Fact]
+    public void _150_逆波兰表达式求值Test()
+    {
+        // var tokens = new string[] { "1", "2", "+", "3", "*" };
+        // var tokens = new string[] { "4", "13", "5", "/", "+" };
+        var tokens = new string[] { "10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+" };
+
+        var res = _150_逆波兰表达式求值.EvalRPN(tokens);
+        // Assert.Equal(9, res);
+        // Assert.Equal(6, res);
+        Assert.Equal(22, res);
+    }
 }
