f8dd59ac780be33a3ddce96cdb12543889499f8c
Memoyu <mmy6076@outlook.com>
2022-03-27 22:41:25 +0800

    fix：二叉搜索树新增节点，值相等时进行覆盖

--- C#/4.二叉树/二叉树/实现二叉搜索树/BinarySearchTree.cs
+++ C#/4.二叉树/二叉树/实现二叉搜索树/BinarySearchTree.cs
@@ -56,6 +56,8 @@ public class BinarySearchTree<T>
             }
             else
             {
+                // 相等时，将覆盖节点的值
+                node.Value = value;
                 return;
             }
         }
