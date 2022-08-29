13f33702cc4fe3462b002e4c77553fe082cfe854
Memoyu <mmy6076@outlook.com>
2022-08-30 00:07:00 +0800

    feat：optimize counting sort

--- C#/算法/1.排序算法/排序算法/8.计数排序/CountingSort.cs
+++ C#/算法/1.排序算法/排序算法/8.计数排序/CountingSort.cs
@@ -4,6 +4,20 @@ public class CountingSort : BaseSort<int>
 {
     protected override void Sort()
     {
+       // 基础版本
+       // Sort_0();
+       // 优化后版本
+       Sort_1();
+    }
+    
+    /// <summary>
+    /// 基础版本
+    /// - 无法对负整数进行排序；
+    /// - 极其浪费内存空间；
+    /// - 是个不稳定的排序；
+    /// </summary>
+    private void Sort_0()
+    {
         // 获取序列最大值
         var max = array[0];
         for (int i = 1; i < array.Length; i++)
@@ -30,6 +44,46 @@ public class CountingSort : BaseSort<int>
                 cur++; // array覆盖索引往后移
                 counts[i]--; // counts值对应的次数 -1
             }
+        }
+    }
+
+    private void Sort_1()
+    {
+        // 获取序列最大值、最小值
+        var max = array[0];
+        var min = array[0];
+        for (int i = 1; i < array.Length; i++)
+        {
+            if (array[i] > max) max = array[i];
+            if (array[i] < min) min = array[i];
+        }
+        
+        // 构造序列值对应出现次数数组
+        var counts = new int[(max - min) + 1];
+        // 值对应次数
+        for (int i = 0; i < array.Length; i++)
+        {
+            // 取出值对应值并进行自增1
+            counts[array[i] - min]++;
+        }
+        // 累加次数
+        for (int i = 1; i < counts.Length; i++)
+        {
+            counts[i] += counts[i - 1];
+        }
+        
+        // 构建有序序列
+        var news = new int[array.Length];
+        for (int i = array.Length - 1; i >= 0; i--)
+        {
+            // 获取值在有序序列中的索引
+            var index = counts[array[i] - min] -= 1;
+            news[index] = array[i];
+        }
+        // 覆盖array
+        for (int i = 0; i < news.Length; i++)
+        {
+            array[i] = news[i];
         }
     }
 }
